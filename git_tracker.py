44a09313bed9d619496207d5c93c0484317b600a
Eduardo Santos <eduardodejesusbr4@gmail.com>
2026-06-07 10:56:44 -0300

    new function: input_error and the first steps of main function

--- git_tracker.py
+++ git_tracker.py
@@ -1,6 +1,17 @@
 import sys
 
 def main():
-    pass        
+    user_input = sys.argv[1]
+    if user_input == "git_hub_activity":
+        git_hub_user = sys.argv[2]
+        
+    else:
+        input_error(sys.argv[2])
+
+def input_error(username):
+    if not username:
+        print("Usage: git_hub_activity <username>")
+    else:
+        print(f"username: {username}, not found :(")
 
 main()
