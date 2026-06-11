c358992b7c708960dcf41e7e9721a5a6f80f496d
Eduardo Santos <eduardodejesusbr4@gmail.com>
2026-06-11 14:14:21 -0300

    Atividade do Usuário - teste

--- git_tracker.py
+++ git_tracker.py
@@ -1,17 +1,28 @@
 import sys
-
+import requests
+import json
+# IP -> domain name DNS
+# https://api.github.com/users/{username}/events/public
 def main():
-    user_input = sys.argv[1]
-    if user_input == "git_hub_activity":
-        git_hub_user = sys.argv[2]
-        
+    try:
+        username = sys.argv[1]
+    except:
+        input_error()
+    
+    output = requests.get(f"https://api.github.com/users/{username}/events/public")
+    if output.status_code == 200:
+        data = output.json()
     else:
-        input_error(sys.argv[2])
+        input_error(username)
+    
+    for activity in data:
+        print(f"Type:{activity["type"]}\nNome do repositório: {activity["repo"]["name"]}\nDescrição: {activity["payload"]["description"]}\n")
 
 def input_error(username):
     if not username:
         print("Usage: git_hub_activity <username>")
     else:
         print(f"username: {username}, not found :(")
+    sys.exit()
 
 main()
